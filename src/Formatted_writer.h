99ec7419e9519626b4113aa764e7d9fa3a909a9f
Jurgas <sebastianjurga7@gmail.com>
2019-04-01 20:47:14 +0200

    Formatted_writer.c /.h utworzenie

---
+++ src/Formatted_writer.h
@@ -0,0 +1,8 @@
+#ifndef LIFE_FORMATTED_WRITER_H
+#define LIFE_FORMATTED_WRITER_H
+
+#include "Grid.h"
+
+void Formatted_Print(char **flags, struct grids *grid_gens);
+
+#endif //LIFE_FORMATTED_WRITER_H
