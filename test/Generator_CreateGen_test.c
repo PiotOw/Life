0069b49b9c417042274dbd32c36798481eb410e6
PiotOw <powczarczyk@vp.pl>
2019-04-14 17:00:02 +0200

    Zmiany spowodowane zmiana typu tablic

--- test/Generator_CreateGen_test.c
+++ test/Generator_CreateGen_test.c
@@ -6,12 +6,12 @@
 
 int Generator_CreateGen_test1() {
     int success = 1;
-    int mini_grid[9];
+    cell_state_t mini_grid[9];
     grids *grid_gens = malloc(sizeof *grid_gens);
     int dim[] = {3, 3};
-    int gridt[] = {0, 1, 0, 0, 1, 0, 0, 1, 0};
-    int gridm[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int gridFinal[] = {0, 0, 0, 1, 1, 1, 0, 0, 0};
+    cell_state_t gridt[] = {0, 1, 0, 0, 1, 0, 0, 1, 0};
+    cell_state_t gridm[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    cell_state_t gridFinal[] = {0, 0, 0, 1, 1, 1, 0, 0, 0};
     grid_gens->grid = gridt;
     grid_gens->new_grid = gridm;
     grid_gens->dim = dim;
@@ -30,12 +30,12 @@ int Generator_CreateGen_test1() {
 
 int Generator_CreateGen_test2() {
     int success = 1;
-    int mini_grid[16];
+    cell_state_t mini_grid[16];
     grids *grid_gens = malloc(sizeof *grid_gens);
     int dim[] = {4, 4};
-    int gridt[] = {0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0};
-    int gridm[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int gridFinal[] = {1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
+    cell_state_t gridt[] = {0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0};
+    cell_state_t gridm[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    cell_state_t gridFinal[] = {1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
     grid_gens->grid = gridt;
     grid_gens->new_grid = gridm;
     grid_gens->dim = dim;
--- test/Rules_Interpreter_test.c
+++ test/Rules_Interpreter_test.c
@@ -3,16 +3,16 @@
 #include "../src/Grid.h"
 
 int Rules_Interpreter_Test1() {
-    int grid[9] = {DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, DEAD, ALIVE, DEAD};
-    int x = Rules_Interpreter(grid);
+    cell_state_t grid[9] = {DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, DEAD, ALIVE, DEAD};
+    cell_state_t x = Rules_Interpreter(grid);
     if (x == DEAD) {
         return 0;
     } else return 1;
 }
 
 int Rules_Interpreter_Test2() {
-    int grid[9] = {DEAD, DEAD, ALIVE, DEAD, ALIVE, DEAD, DEAD, ALIVE, ALIVE};
-    x = Rules_Interpreter(grid);
+    cell_state_t grid[9] = {DEAD, DEAD, ALIVE, DEAD, ALIVE, DEAD, DEAD, ALIVE, ALIVE};
+    cell_state_t x = Rules_Interpreter(grid);
     if (x == ALIVE) {
         return 0;
     } else return 1;
